438be0d939cca6b4fe283941ec42b7d0c27e624c
Matvei Nazaruk <matvei.nazaruk@gmail.com>
2015-02-18 11:53:44 +0300

    model of event and publication added

--- src/main/java/by/bsu/famcs/model/Event.java
+++ src/main/java/by/bsu/famcs/model/Event.java
@@ -1,7 +1,35 @@
 package by.bsu.famcs.model;
 
+import org.joda.time.DateTime;
+
 /**
  * Created by matvei on 18.02.15.
  */
 public class Event {
+    private String name;
+    private DateTime date;
+
+    public Event() {
+    }
+
+    public Event(String name, DateTime date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DateTime getDate() {
+        return date;
+    }
+
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
 }
--- src/main/java/by/bsu/famcs/model/Publication.java
+++ src/main/java/by/bsu/famcs/model/Publication.java
@@ -1,7 +1,45 @@
 package by.bsu.famcs.model;
 
+import org.joda.time.DateTime;
+
 /**
  * Created by matvei on 18.02.15.
  */
 public class Publication {
+    private String title;
+    private String content;
+    private DateTime publicationDate;
+
+    public Publication() {
+    }
+
+    public Publication(String title, String content, DateTime publicationDate) {
+        this.title = title;
+        this.content = content;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public DateTime getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(DateTime publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 }
