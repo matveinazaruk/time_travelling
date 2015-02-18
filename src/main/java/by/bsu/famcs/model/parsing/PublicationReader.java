d0f5e96ae8f6eff43e58bed8b7c2b3e6b72a7b33
IvanBorisevich <borisevich.ivan1994@gmail.com>
2015-02-18 17:12:55 +0300

    publication read

--- src/main/java/by/bsu/famcs/model/parsing/PublicationReader.java
+++ src/main/java/by/bsu/famcs/model/parsing/PublicationReader.java
@@ -2,11 +2,15 @@ package by.bsu.famcs.model.parsing;
 
 import by.bsu.famcs.model.entities.Publication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.*;
+import java.io.InputStreamReader;
 import java.util.List;
 
-/**
- * Created by matvei on 18.02.15.
- */
 public class PublicationReader {
     private static PublicationReader reader = new PublicationReader();
 
@@ -18,6 +22,35 @@ public class PublicationReader {
 
     // Тут томасу надо считать из файла и вернуть ваньке публикации
     public List<Publication> getPublications() {
+
+
         return null;
+    }
+
+    public String readPublication(String fileName) {
+
+        Path path = Paths.get(fileName);
+        try {
+            List<String> lines = Files.readAllLines(path);
+            final StringBuilder resultJson = new StringBuilder("");
+            lines.forEach(resultJson::append);
+            return resultJson.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+//
+//    private Publication parsePublication(String pub) {
+//
+//    }
+
+    public static void main(String[] args) {
+
+        PublicationReader reader = PublicationReader.getInstance();
+        String result = reader.readPublication("article.json");
+        System.out.println(result);
     }
 }
